da1561bba79b31a7843b4f0b1383053035b23028
p.lukyanov <p.lukyanov@qa-academy.by>
2022-06-24 10:56:25 +0300

    Commit - 4

--- src/post/post.tsx
+++ src/post/post.tsx
@@ -4,6 +4,9 @@ export const Post = () => {
     return (
         <div>
             <h2>Post333</h2>
+            <h2>Post333</h2>
+            <h2>Post333</h2>
+            <h2>Post333</h2>
         </div>
     );
 };
